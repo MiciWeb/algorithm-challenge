498cb9c0782980fa03bfbf7d25eb239b6d23cd2a
Mici Srsr <micipsa.sersour@epitech.eu>
2021-08-23 22:47:32 +0200

    add whereMyAnagrams cleaner solution

--- whereMyAnagrams.js
+++ whereMyAnagrams.js
@@ -1,13 +1,4 @@
 function anagrams(word, words) {
-    let split;
-    let join;
-    let arr = [];
     let correct = word.split("").sort().join("")
-    for (let i = 0; i < words.length; i++) {
-        split = words[i].split("")
-        console.log(split)
-        join = split.sort().join("")
-        join == correct ? arr.push(words[i]) : []
-    }
-    return arr
+    return words.filter(item => item.split("").sort().join("") === correct)
 }
