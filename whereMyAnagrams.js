3cb002ebcf1abe849a28695a6eabc06568dd3b59
Mici Srsr <micipsa.sersour@epitech.eu>
2021-08-24 14:51:45 +0200

    add mexicanWave solution

--- whereMyAnagrams.js
+++ whereMyAnagrams.js
@@ -1,3 +1,6 @@
+// Write a function that will find all the anagrams of a word from a list. 
+// You will be given two inputs a word and an array with words.
+// You should return an array of all the anagrams or an empty array if there are none. 
 function anagrams(word, words) {
     let correct = word.split("").sort().join("")
     return words.filter(item => item.split("").sort().join("") === correct)
