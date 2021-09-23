dd553839db3bf43946b59ff84df7165141444b15
Mici Srsr <83211610+MiciWeb@users.noreply.github.com>
2021-09-23 14:59:07 +0200

    Update #7maxSeniorAge.js

--- #7maxSeniorAge.js
+++ #7maxSeniorAge.js
@@ -1,4 +1,5 @@
-
+// This exercices is from a coding series about higher-order function
+// https://www.codewars.com/collections/coding-meetup-series
 function findSenior(list) {
   let age =  Math.max(...list.map(item=>item.age))
   return list.filter(item=>item.age == age )
